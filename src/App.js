02377e2dce52510c9a95b04abbcf32039038ec73
nurgiz_increpo <increpo@gmail.com>
2023-06-16 12:20:55 +0500

    v1.0.6

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import './App.css';
 import {useEffect} from 'react';
 import {useTelegram} from './components/hooks/useTelegram.js';
+import Header from './components/Header/Header.jsx';
 
 function App() {
     const {tg, onToggleButton} = useTelegram();
@@ -11,6 +12,7 @@ function App() {
 
     return (
         <div className="App">
+            <Header />
             <button onClick={onToggleButton}>Toggle</button>
         </div>
     );
--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import Button from '../Button/Button';
-import {useTelegram} from 'src/components/hooks/useTelegram';
+import {useTelegram} from '../hooks/useTelegram';
 
 const Header = () => {
 	const {user, onClose} = useTelegram();
