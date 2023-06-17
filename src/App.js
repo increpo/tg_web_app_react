4679780ea3b98df98bb02e24cabe18aea84e8167
nurgiz_increpo <increpo@gmail.com>
2023-06-17 14:02:02 +0500

    v1.0.8

--- src/App.js
+++ src/App.js
@@ -1,7 +1,10 @@
-import './App.css';
 import {useEffect} from 'react';
+import {Route, Routes} from 'react-router-dom';
+import './App.css';
 import {useTelegram} from './components/hooks/useTelegram.js';
 import Header from './components/Header/Header.jsx';
+import ProductList from './components/ProductList/ProductList.jsx';
+import Form from './components/Form/Form.jsx';
 
 function App() {
     const {tg, onToggleButton} = useTelegram();
@@ -13,7 +16,10 @@ function App() {
     return (
         <div className="App">
             <Header />
-            <button onClick={onToggleButton}>Toggle</button>
+            <Routes>
+                <Route index element={<ProductList />}/>
+                <Route path={'form'} element={<Form />}/>
+            </Routes>
         </div>
     );
 }
---
+++ src/components/Form/Form.jsx
@@ -0,0 +1,63 @@
+import React, {useState, useEffect} from 'react';
+import './Form.css';
+import {useTelegram} from '../hooks/useTelegram';
+
+const Form = () => {
+	const [country, setCountry] = useState('');
+	const [street, setStreet] = useState('');
+	const [subject, setSubject] = useState('physical');
+	const {tg} = useTelegram();
+
+	useEffect( () => {
+		tg.MainButton.setParams({
+			text: 'Send data'
+		})
+	}, [])
+
+	useEffect( () => {
+		if(!country || !street){
+			tg.MainButton.hide();
+		} else {
+			tg.MainButton.show();
+		}
+	}, [country, street])
+
+	const onChangeCountry = (e) => {
+		setCountry(e.target.value);
+	}
+	const onChangeStreet = (e) => {
+		setStreet(e.target.value);
+	}
+	const onChangeSubject = (e) => {
+		setSubject(e.target.value);
+	}
+
+	return (
+		<div className={'form'}>
+			<h3>Enter your details</h3>
+			<input
+				className={'input'} 
+				type='text'
+				placeholder={'Country'}
+				value={country}
+				onChange={onChangeCountry}
+			/>
+			<input
+				className={'input'}
+				type='text'
+				placeholder={'Street'}
+				value={street}
+				onChange={onChangeStreet}
+			/>
+			<select name={'select'}
+					value={subject}
+					onChange={onChangeSubject}
+			>
+				<option value={'physical'}>Physical person</option>
+				<option value={'legal'}>Legal person</option>
+			</select>
+		</div>
+	)
+}
+
+export default Form;
