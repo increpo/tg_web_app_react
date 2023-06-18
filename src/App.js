7aa7360b2dc09a6c9721c7662185009a0be7ff35
nurgiz_increpo <increpo@gmail.com>
2023-06-18 17:34:54 +0500

    v2.0.4

--- src/App.js
+++ src/App.js
@@ -1,12 +1,11 @@
-import {useEffect} from 'react';
+import {useEffect, useState} from 'react';
 import {Route, Routes} from 'react-router-dom';
 import './App.css';
 import {useTelegram} from './hooks/useTelegram.js';
 import Header from './components/Header/Header.jsx';
 import ProductList from './components/ProductList/ProductList.jsx';
 import Form from './components/Form/Form.jsx';
-//import QRScanner from './components/QRScanner/QRScanner.jsx';
-import QRScanner2 from './components/QRScanner2/QRScanner2.jsx';
+import QRScanner from './components/QRScanner/QRScanner.jsx';
 
 function App() {
     const {tg, onToggleButton} = useTelegram();
@@ -15,17 +14,28 @@ function App() {
         tg.ready();
     }, []);
 
+    const [scanResult, setScanResult] = useState(null);
+
     const onNewScanResult = (decodedText, decodedResult) => {
+        setScanResult(decodedResult);
         // handle decoded results here
-        tg.sendData(decodedText);
-        console.log(decodedText);
+        // tg.sendData(decodedText);
+        // console.log(decodedText);
     };
 
     return (
         <div className="App">
             <Header />
-
-            <QRScanner2 />
+            <QRScanner
+                fps={10}
+                qrbox={250}
+                disableFlip={false}
+                qrCodeSuccessCallback={onNewScanResult}
+            />
+            { scanResult
+                ? <div><a href={scanResult}>Click me</a>{scanResult}</div>
+                : <div id="qr_scanner_render"></div>
+            }
             <Routes>
                 <Route index element={<ProductList />}/>
                 <Route path={'form'} element={<Form />}/>
@@ -35,10 +45,3 @@ function App() {
 }
 
 export default App;
-
-            // <QRScanner
-            //     fps={10}
-            //     qrbox={250}
-            //     disableFlip={false}
-            //     qrCodeSuccessCallback={onNewScanResult}
-            // />
