26562525cb89222ebb8824b2fc74b71066518dc5
nurgiz_increpo <increpo@gmail.com>
2023-06-18 18:48:32 +0500

    v2.0.13

--- src/components/QRScanner/QRScanner.jsx
+++ src/components/QRScanner/QRScanner.jsx
@@ -41,7 +41,7 @@ const QRScanner = (props) => {
         // if (!(props.qrCodeSuccessCallback)) {
         //     throw "qrCodeSuccessCallback is required callback.";
         // }
-        const html5QrcodeScanner = new Html5QrcodeScanner(qrcodeRegionId, config, verbose);
+        //const html5QrcodeScanner = new Html5QrcodeScanner(qrcodeRegionId, config, verbose);
         // html5QrcodeScanner.render(props.qrCodeSuccessCallback, props.qrCodeErrorCallback);
 
 	    const onCodeSuccessCallback = (decodedText, decodedResult) => {
@@ -58,13 +58,50 @@ const QRScanner = (props) => {
 	        console.log(e);
 	    };
 
-        html5QrcodeScanner.render(onCodeSuccessCallback, onCodeErrorCallback);
+
+        //html5QrcodeScanner.render(onCodeSuccessCallback, onCodeErrorCallback);
+
+	    const html5QrCode = new Html5Qrcode(/* element id */ qrcodeRegionId, verbose);
+
+		Html5Qrcode.getCameras().then(devices => {
+		  /**
+		   * devices would be an array of objects of type:
+		   * { id: "id", label: "label" }
+		   */
+		  if (devices && devices.length) {
+		    var cameraId = devices[devices.length - 1].id;
+		    // .. use this to start scanning.
+			html5QrCode.start(
+			  cameraId, 
+			  {
+			    fps: 10,    // Optional, frame per seconds for qr code scanning
+			    qrbox: { width: 250, height: 250 }  // Optional, if you want bounded box UI
+			  },
+			  (decodedText, decodedResult) => {
+			  	setScanResult(decodedText);
+			    // do something when code is read
+			  },
+			  (errorMessage) => {
+			    // parse error, ignore it.
+			  })
+			.catch((err) => {
+			  // Start failed, handle it.
+			});
+		  }
+		}).catch(err => {
+		  // handle err
+		});
 
         // cleanup function when component will unmount
         return () => {
-            html5QrcodeScanner.clear().catch(error => {
-                console.error("Failed to clear html5QrcodeScanner. ", error);
-            });
+            // html5QrcodeScanner.clear().catch(error => {
+            //     console.error("Failed to clear html5QrcodeScanner. ", error);
+            // });
+            // html5QrCode.stop().then((ignore) => {
+			//   // QR Code scanning is stopped.
+			// }).catch((err) => {
+			//   // Stop failed, handle it.
+			// });
         };
     }, []);
 
