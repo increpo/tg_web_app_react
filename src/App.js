b02bd357d297495844228cf585f7c8858923e0af
nurgiz_increpo <increpo@gmail.com>
2023-06-18 16:40:05 +0500

    v2.0.0

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import {useTelegram} from './hooks/useTelegram.js';
 import Header from './components/Header/Header.jsx';
 import ProductList from './components/ProductList/ProductList.jsx';
 import Form from './components/Form/Form.jsx';
+import QRScanner from './components/QRScanner/QRScanner.jsx';
 
 function App() {
     const {tg, onToggleButton} = useTelegram();
@@ -13,9 +14,20 @@ function App() {
         tg.ready();
     }, []);
 
+    const onNewScanResult = (decodedText, decodedResult) => {
+        // handle decoded results here
+        console.log(decodedText);
+    };
+
     return (
         <div className="App">
             <Header />
+            <QRScanner
+                fps={10}
+                qrbox={250}
+                disableFlip={false}
+                qrCodeSuccessCallback={onNewScanResult}
+            />
             <Routes>
                 <Route index element={<ProductList />}/>
                 <Route path={'form'} element={<Form />}/>
