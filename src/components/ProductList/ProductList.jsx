7c21df6f20b036d56b1b553bfd24758670b7f5eb
nurgiz_increpo <increpo@gmail.com>
2023-06-18 12:57:24 +0500

    v1.0.24

--- src/components/ProductList/ProductList.jsx
+++ src/components/ProductList/ProductList.jsx
@@ -30,7 +30,7 @@ const ProductList = () => {
 			totalPrice: getTotalPrice(addedItems),
 			queryId
 		}
-		fetch('http://localhost:8000', {
+		fetch('http://94.158.61.52:8000/web-data', {
 			method: 'POST',
 			headers: {
 				'Content-Type': 'Application/json'
