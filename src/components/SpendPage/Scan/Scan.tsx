265b89f926a3264fff8985bcf9d17404545e79b9
nurgiz_increpo <increpo@gmail.com>
2023-06-20 13:38:48 +0500

    trabelingo_bot_test_v4_artem_1445_v_5

--- src/components/SpendPage/Scan/Scan.tsx
+++ src/components/SpendPage/Scan/Scan.tsx
@@ -1,13 +1,15 @@
 import { FC } from "react";
 import { Link } from 'react-router-dom';
 import style from './Scan.module.scss';
-import image from './scan_image.png';
+// import image from './scan_image.png';
+import QRScan from "./QRScan.jsx";
 
 const Scan: FC = () => {
 
   return (
     <Link to='/spendpage/scanqr' className={style.wrap}>
-      <img src={image} alt='QR code' className={style.image}/>
+      {/*<img src={image} alt='QR code' className={style.image}/>*/}
+      <QRScan className={style.qrscan} />
     </Link>
   );
 }
