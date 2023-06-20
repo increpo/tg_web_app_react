bbecb92014c0057431017de8659af151717207f1
nurgiz_increpo <increpo@gmail.com>
2023-06-20 13:11:43 +0500

    trabelingo_bot_test_v4_artem_1445

--- src/components/GetPage/GetPage.tsx
+++ src/components/GetPage/GetPage.tsx
@@ -1,37 +1,19 @@
-import React, { FC, useState } from "react";
+import { FC } from "react";
 import BackBtn from "../common/BackBtn/BackBtn";
 import style from './GetPage.module.scss';
 
-import { QRCode } from 'react-qrcode-logo';
-import ShareBtn from './ShareBtn.js';
-
 const GetPage: FC = () => {
-  const qrUrl = 'https://makeshop.pro/';
-  const imageUrl = '../images/logo_qr.png';
-  const clipboardIcon = '../images/clipboard_icon.png';
 
   return (
     <div className={style.wrap}>
       <div className={style.header}>
         <BackBtn inversColor />
+
       </div>
       <div className={style.content}>
         <div className={style.title}>Show this code for crediting coins</div>
         <div className={style.qr}>
-          <QRCode
-            value={qrUrl}            // here you should keep the link/value(string) for which you are generation promocode
-            size={250}              // the dimension of the QR code (number)
-            logoImage={imageUrl}  // URL of the logo you want to use, make sure it is a dynamic url
-            logoHeight={50}
-            logoWidth={50}
-            logoOpacity={1}
-            removeQrCodeBehindLogo={true}
-            enableCORS={true}       // enabling CORS, this is the thing that will bypass that DOM check
-            qrStyle="squares"          // type of qr code, wether you want dotted ones or the square ones (dots / squares)
-            eyeRadius={3}          // radius of the promocode eye
-            id={"logo_id"}
-          />
-{/*          <svg width="250" height="250" viewBox="0 0 250 250" fill="none" xmlns="http://www.w3.org/2000/svg">
+          <svg width="250" height="250" viewBox="0 0 250 250" fill="none" xmlns="http://www.w3.org/2000/svg">
             <rect width="250" height="250" fill="white" />
             <path d="M17.2414 17.2412H25.8621V25.8619H17.2414V17.2412Z" fill="black" />
             <path d="M25.8621 17.2412H34.4828V25.8619H25.8621V17.2412Z" fill="black" />
@@ -338,26 +320,20 @@ const GetPage: FC = () => {
               <path d="M124.594 122.219C124.531 125.74 124.5 128.578 124.5 130.734C124.5 131.818 124.661 132.568 124.984 132.984C125.307 133.391 125.922 133.594 126.828 133.594C127.547 133.594 128.484 133.5 129.641 133.312L129.734 134.016C127.932 134.922 126.323 135.375 124.906 135.375C123.771 135.375 122.927 135.073 122.375 134.469C121.823 133.854 121.547 132.932 121.547 131.703C121.547 129.682 121.583 126.49 121.656 122.125L119.5 121.594L119.406 120.953L121.812 120.75L122.891 116.969C123.474 116.812 124.005 116.661 124.484 116.516L124.641 116.625C124.609 117.885 124.594 119.062 124.594 120.156V120.734H129.297V122.219H124.594Z" fill="white" />
             </g>
           </svg>
-*/}
         </div>
         <div className={style.address}>352 - 628</div>
         <div className={style.footer}>
           <div className={style.share}>
-{/*            <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+            <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
               <path fill-rule="evenodd" clip-rule="evenodd" d="M11.4802 2.06732C11.2842 2.16787 6.71644 6.73948 6.61226 6.93947C6.4435 7.2634 6.53483 7.61549 6.84772 7.8472C7.01806 7.97337 7.33286 7.99939 7.5242 7.90318C7.60923 7.86041 8.45777 7.04898 9.40981 6.10002L11.1408 4.37462L11.1418 10.7322C11.1428 16.8325 11.1462 17.0973 11.227 17.2751C11.3393 17.5224 11.5838 17.6729 11.8732 17.6729C12.1195 17.6729 12.2781 17.5798 12.4419 17.3391L12.5437 17.1895V10.7821V4.37457L14.32 6.14686C16.2412 8.06374 16.1938 8.02627 16.6022 7.94966C16.8153 7.9097 17.0466 7.68021 17.1096 7.44624C17.2162 7.0503 17.2755 7.12184 14.6783 4.51545C13.3142 3.14652 12.2326 2.09655 12.1492 2.06035C11.9577 1.97721 11.6497 1.98038 11.4802 2.06732ZM2.42917 13.0128C2.26272 13.0868 2.11175 13.2462 2.04753 13.4157C2.01513 13.5012 1.99951 14.4918 2.00001 16.4252C2.00092 19.6513 2.00164 19.6601 2.31159 20.2971C2.70391 21.1034 3.4213 21.6887 4.28957 21.9108C4.62962 21.9977 4.82046 22 11.7744 22C18.7283 22 18.9192 21.9977 19.2592 21.9108C20.13 21.6881 20.8448 21.1053 21.2317 20.3029C21.5533 19.6359 21.5541 19.6253 21.5397 16.3351C21.5272 13.4595 21.5253 13.4059 21.4336 13.2831C21.2904 13.0914 21.13 12.99 20.9265 12.9627C20.6664 12.9278 20.3756 13.0743 20.2455 13.3058C20.1468 13.4814 20.1466 13.4872 20.1466 16.2678C20.1466 19.1532 20.1355 19.3548 19.9558 19.7097C19.8088 20.0003 19.4917 20.3022 19.1842 20.4441L18.9021 20.5745H11.7744H4.64668L4.36456 20.4441C4.05714 20.3022 3.73999 20.0003 3.59296 19.7097C3.41334 19.3548 3.40216 19.1532 3.40216 16.2678C3.40216 13.4872 3.40198 13.4814 3.30332 13.3058C3.13529 13.0069 2.74039 12.8745 2.42917 13.0128Z" fill="black" />
-            </svg>*/}
-            <ShareBtn url={qrUrl} />
+            </svg>
+            Share
           </div>
           <div className={style.copy}>
-{/*            <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+            <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
               <path fill-rule="evenodd" clip-rule="evenodd" d="M7.54843 2.11031C7.30719 2.23051 7.11156 2.46007 7.04042 2.70631C7.01117 2.8077 7.00116 5.03361 7.00946 9.59747L7.02177 16.3388L7.14802 16.5431C7.22629 16.6697 7.35196 16.7953 7.47854 16.8736L7.68277 16.9998H14.5109H21.339L21.5432 16.8736C21.6698 16.7953 21.7955 16.6697 21.8737 16.5431L22 16.3388V9.51073V2.68262L21.8737 2.47839C21.7955 2.3518 21.6698 2.22613 21.5432 2.14786L21.339 2.02161L14.5544 2.01081L7.76976 2L7.54843 2.11031ZM20.6858 9.51073V15.6856H14.5109H8.33602V9.51073V3.33586H14.5109H20.6858V9.51073ZM2.54103 7.11734C2.30054 7.23716 2.10482 7.46705 2.03377 7.71296C2.00452 7.81435 1.99451 10.0403 2.00281 14.6041L2.01512 21.3455L2.14137 21.5497C2.21964 21.6763 2.34531 21.802 2.47189 21.8802L2.67612 22.0065H9.50423H16.3323L16.5366 21.8802C16.6631 21.802 16.7889 21.6763 16.8671 21.5498L16.9934 21.3457L17.0053 19.8507L17.0172 18.3558H16.3482H15.6791V19.524V20.6922H9.50423H3.32937V14.5174V8.34251H4.49758H5.6658V7.67496V7.0074H4.21371H2.76165L2.54103 7.11734Z" fill="black" />
-            </svg>*/}
-            <button className={style.clipboardBtn}
-              onClick={() =>  {navigator.clipboard.writeText('Copy this text to clipboard'); alert('Link copied')}}
-            >
-                <img src={clipboardIcon} alt="clipboard icon" />
-                <span className={style.clipboardBtnText}>Copy link</span>
-            </button>
+            </svg>
+            Copy link
           </div>
         </div>
       </div>
