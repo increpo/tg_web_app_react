57095659b5b6c5b5bead1e7976c51397c33760d3
nurgiz_increpo <increpo@gmail.com>
2023-06-26 20:39:30 +0500

    added user info in main page

--- src/components/MainPage/MainPage.tsx
+++ src/components/MainPage/MainPage.tsx
@@ -4,11 +4,16 @@ import CoinsBalance from "./CoinsBalance/CoinsBalance";
 import MainBtns from "./MainBtns/MainBtns";
 import HorizontalLine from "../UI/HorizontalLine/HorizontalLine";
 import RecentActivity from "./RecentActivity/RecentActivity";
+import {useTelegram} from '../../hooks/useTelegram';
 
 const MainPage: FC = () => {
 
+  const {tg} = useTelegram();
+  const userData = JSON.stringify(tg.initDataUnsafe);
+  
   return (
     <div>
+      <div>{userData}</div>
       <div className={style.wrap}>
         <CoinsBalance />
       </div>
