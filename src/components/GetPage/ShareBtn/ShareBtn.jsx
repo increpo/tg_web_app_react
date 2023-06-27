9b50c21c8165ce068e66e92929fbb3bd80752997
nurgiz_increpo <increpo@gmail.com>
2023-06-27 21:27:03 +0500

    added custom popup with social btns

--- src/components/GetPage/ShareBtn/ShareBtn.jsx
+++ src/components/GetPage/ShareBtn/ShareBtn.jsx
@@ -2,21 +2,21 @@ import './ShareBtn.scss';
  //пользователь 18 или 20
 import {
   FacebookShareButton,
-  GooglePlusShareButton,
+  // GooglePlusShareButton,
   LinkedinShareButton,
   TwitterShareButton,
   TelegramShareButton,
   WhatsappShareButton,
-  PinterestShareButton,
+  // PinterestShareButton,
   VKShareButton,
-  OKShareButton,
-  RedditShareButton,
-  TumblrShareButton,
-  LivejournalShareButton,
+  // OKShareButton,
+  // RedditShareButton,
+  // TumblrShareButton,
+  // LivejournalShareButton,
   MailruShareButton,
   ViberShareButton,
-  WorkplaceShareButton,
-  EmailShareButton,
+  // WorkplaceShareButton,
+  // EmailShareButton,
 } from 'react-share';
 
 import {
@@ -24,19 +24,157 @@ import {
   TwitterIcon,
   TelegramIcon,
   WhatsappIcon,
-  GooglePlusIcon,
+  // GooglePlusIcon,
   LinkedinIcon,
-  PinterestIcon,
+  // PinterestIcon,
   VKIcon,
-  OKIcon,
-  RedditIcon,
-  TumblrIcon,
-  LivejournalIcon,
+  // OKIcon,
+  // RedditIcon,
+  // TumblrIcon,
+  // LivejournalIcon,
   MailruIcon,
   ViberIcon,
-  WorkplaceIcon,
-  EmailIcon,
+  // WorkplaceIcon,
+  // EmailIcon,
 } from 'react-share';
+
+
+import React, { useState, createElement } from 'react';
+import ReactDOM from 'react-dom';
+import 'react-responsive-modal/styles.css';
+import { Modal } from 'react-responsive-modal';
+
+const shareUrl = 'https://makeshop.pro';
+
+const ShareBtn = ({shareLink, className}) => {
+  const [open, setOpen] = useState(false);
+
+  const onOpenModal = () => setOpen(true);
+  const onCloseModal = () => setOpen(false);
+  const socialBtns = ['Facebook', 'Telegram', 'Twitter', 'Whatsapp'];
+
+  return (
+    <div>
+      <button className={className} onClick={onOpenModal}>Open modal</button>
+      <Modal
+      	open={open}							//boolean		Control if the modal is open or not.
+      	onClose={onCloseModal}				
+      	closeOnEsc={true}					//boolean	true	Is the modal closable when user press esc key.
+      	closeOnOverlayClick={true}			//boolean	true	Is the modal closable when user click on overlay.
+      	center={true}						//boolean	false	Should the dialog be centered.
+      	closeIcon={''}						
+      	blockScroll={true}					//boolean	true	Whether to block scrolling when dialog is open.
+      	showCloseIcon={false}				//boolean	true	Show the close icon.
+      	closeIconId={'social_modal_close'}	//string		id attribute for the close icon button.
+      	//closeIcon							//React.ReactNode		Custom icon to render (svg, img, etc...).
+      	//focusTrapped={true}				//boolean	true	When the modal is open, trap focus within it.
+      	//initialFocusRef					//React.RefElement<HTMLElement> - Sets focus on this specific element when modal opens if focus trap is used.
+      	//container							//Element		You can specify a container prop which should be of type Element. The portal will be rendered inside that element. The default behavior will create a div node and render it at the at the end of document.body.
+      	//classNames						//An object containing classNames to style the modal.{ root?: string; overlay?: string; overlayAnimationIn?: string; overlayAnimationOut?: string; modal?: string; modalAnimationIn?: string; modalAnimationOut?: string; closeButton?: string; closeIcon?: string; }
+      	//styles							//An object containing the styles objects to style the modal. { root?: React.CSSProperties; overlay?: React.CSSProperties; overlay?: React.CSSProperties; modalContainer?: React.CSSProperties; modal?: React.CSSProperties; closeButton?: React.CSSProperties; closeIcon?: React.CSSProperties; }
+      	//animationDuration={300}			//Animation duration in milliseconds.
+      	//role={"dialog"}					//ARIA role for modal
+      	//RefElement						//React.RefElement<HTMLDivElement>	undefined	Ref for modal dialog element
+      	//ariaLabelledby					//string		ARIA label for modal
+      	//ariaDescribedby					//string		ARIA description for modal
+      	//modalId							//string		id attribute for modal
+      	>
+	 		<div className="social-buttons">
+	 			<div>
+	 				<p>Facebook</p>
+		 			<FacebookShareButton
+		 				url={shareUrl}
+		 		        quote={'Dummy text!'}
+		         		hashtag="#muo"
+		 			>
+		 				
+		 				<FacebookIcon size={40} round />
+		 			</FacebookShareButton>	 				
+	 			</div>
+	 			<div>
+	 				<p>Telegram</p>
+		 			<TelegramShareButton
+		 				url={shareUrl}
+		 		        quote={'Dummy text!'}
+		         		hashtag="#muo"
+		 			>
+		 				
+		 				<TelegramIcon size={40} round />
+		 			</TelegramShareButton>	 				
+	 			</div>
+	 			<div>
+	 				<p>Twitter</p>
+		 			<TwitterShareButton
+		 				url={shareUrl}
+		 		        quote={'Dummy text!'}
+		         		hashtag="#muo"
+		 			>
+		 				
+		 				<TwitterIcon size={40} round />
+		 			</TwitterShareButton>	 				
+	 			</div>
+	 			<div>
+	 				<p>Whatsapp</p>
+		 			<WhatsappShareButton
+		 				url={shareUrl}
+		 		        quote={'Dummy text!'}
+		         		hashtag="#muo"
+		 			>
+		 				
+		 				<WhatsappIcon size={40} round />
+		 			</WhatsappShareButton>	 				
+	 			</div>
+	 			<div>
+	 				<p>Viber</p>
+		 			<ViberShareButton
+		 				url={shareUrl}
+		 		        quote={'Dummy text!'}
+		         		hashtag="#muo"
+		 			>
+		 				
+		 				<ViberIcon size={40} round />
+		 			</ViberShareButton>	 				
+	 			</div>
+	 			<div>
+	 				<p>VK</p>
+		 			<VKShareButton
+		 				url={shareUrl}
+		 		        quote={'Dummy text!'}
+		         		hashtag="#muo"
+		 			>
+		 				
+		 				<VKIcon size={40} round />
+		 			</VKShareButton>	 				
+	 			</div>
+	 			<div>
+	 				<p>Linkedin</p>
+		 			<LinkedinShareButton
+		 				url={shareUrl}
+		 		        quote={'Dummy text!'}
+		         		hashtag="#muo"
+		 			>
+		 				
+		 				<LinkedinIcon size={40} round />
+		 			</LinkedinShareButton>	 				
+	 			</div>
+	 			<div>
+	 				<p>Mailru</p>
+		 			<MailruShareButton
+		 				url={shareUrl}
+		 		        quote={'Dummy text!'}
+		         		hashtag="#muo"
+		 			>
+		 				
+		 				<MailruIcon size={40} round />
+		 			</MailruShareButton>	 				
+	 			</div>
+	 		</div>
+      </Modal>
+    </div>
+  );
+};
+
+export default ShareBtn;
 
 
 // const ShareBtn = () => {
@@ -111,96 +249,3 @@ import {
 //     	</div>
 //   	</Popup>
 // );
-
-import React, { useState, createElement } from 'react';
-import ReactDOM from 'react-dom';
-import 'react-responsive-modal/styles.css';
-import { Modal } from 'react-responsive-modal';
-
-const shareUrl = 'https://makeshop.pro';
-
-const ShareBtn = ({shareLink, className}) => {
-  const [open, setOpen] = useState(false);
-
-  const onOpenModal = () => setOpen(true);
-  const onCloseModal = () => setOpen(false);
-  const socialBtns = ['Facebook', 'Telegram', 'Twitter', 'Whatsapp'];
-
-  return (
-    <div>
-      <button className={className} onClick={onOpenModal}>Open modal</button>
-      <Modal
-      	open={open}							//boolean		Control if the modal is open or not.
-      	onClose={onCloseModal}				
-      	closeOnEsc={true}					//boolean	true	Is the modal closable when user press esc key.
-      	closeOnOverlayClick={true}			//boolean	true	Is the modal closable when user click on overlay.
-      	center={true}						//boolean	false	Should the dialog be centered.
-      	closeIcon={''}						
-      	blockScroll={true}					//boolean	true	Whether to block scrolling when dialog is open.
-      	showCloseIcon={false}				//boolean	true	Show the close icon.
-      	closeIconId={'social_modal_close'}	//string		id attribute for the close icon button.
-      	//closeIcon							//React.ReactNode		Custom icon to render (svg, img, etc...).
-      	//focusTrapped={true}				//boolean	true	When the modal is open, trap focus within it.
-      	//initialFocusRef					//React.RefElement<HTMLElement> - Sets focus on this specific element when modal opens if focus trap is used.
-      	//container							//Element		You can specify a container prop which should be of type Element. The portal will be rendered inside that element. The default behavior will create a div node and render it at the at the end of document.body.
-      	//classNames						//An object containing classNames to style the modal.{ root?: string; overlay?: string; overlayAnimationIn?: string; overlayAnimationOut?: string; modal?: string; modalAnimationIn?: string; modalAnimationOut?: string; closeButton?: string; closeIcon?: string; }
-      	//styles							//An object containing the styles objects to style the modal. { root?: React.CSSProperties; overlay?: React.CSSProperties; overlay?: React.CSSProperties; modalContainer?: React.CSSProperties; modal?: React.CSSProperties; closeButton?: React.CSSProperties; closeIcon?: React.CSSProperties; }
-      	//animationDuration={300}			//Animation duration in milliseconds.
-      	//role={"dialog"}					//ARIA role for modal
-      	//RefElement						//React.RefElement<HTMLDivElement>	undefined	Ref for modal dialog element
-      	//ariaLabelledby					//string		ARIA label for modal
-      	//ariaDescribedby					//string		ARIA description for modal
-      	//modalId							//string		id attribute for modal
-      	>
-	 		<div className="social-buttons">
-	 			<div>
-	 				<p>facebook</p>
-		 			<FacebookShareButton
-		 				url={shareUrl}
-		 		        quote={'Dummy text!'}
-		         		hashtag="#muo"
-		 			>
-		 				
-		 				<FacebookIcon size={40} round />
-		 			</FacebookShareButton>	 				
-	 			</div>
-	 			<div>
-	 				<p>telegram</p>
-		 			<TelegramShareButton
-		 				url={shareUrl}
-		 		        quote={'Dummy text!'}
-		         		hashtag="#muo"
-		 			>
-		 				
-		 				<TelegramIcon size={40} round />
-		 			</TelegramShareButton>	 				
-	 			</div>
-	 			<div>
-	 				<p>Twitter</p>
-		 			<TwitterShareButton
-		 				url={shareUrl}
-		 		        quote={'Dummy text!'}
-		         		hashtag="#muo"
-		 			>
-		 				
-		 				<TwitterIcon size={40} round />
-		 			</TwitterShareButton>	 				
-	 			</div>
-	 			<div>
-	 				<p>Whatsapp</p>
-		 			<WhatsappShareButton
-		 				url={shareUrl}
-		 		        quote={'Dummy text!'}
-		         		hashtag="#muo"
-		 			>
-		 				
-		 				<WhatsappIcon size={40} round />
-		 			</WhatsappShareButton>	 				
-	 			</div>
-	 		</div>
-      </Modal>
-    </div>
-  );
-};
-
-export default ShareBtn;
